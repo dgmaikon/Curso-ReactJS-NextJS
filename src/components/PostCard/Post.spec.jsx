1d163d088180110c9399ff29d25b14fae16b4e75
dgmaikon <dgmaikondev@gmail.com>
2023-07-24 13:34:54 -0300

    Ref: cleaning code.

--- src/components/PostCard/Post.spec.jsx
+++ src/components/PostCard/Post.spec.jsx
@@ -1,24 +1,21 @@
-import { render, screen } from "@testing-library/react";
-import { describe } from "vitest";
-import { PostCard } from ".";
-import { PostCardPropsMock } from "./mock";
+import { render, screen } from '@testing-library/react';
+import { describe } from 'vitest';
+import { PostCard } from '.';
+import { PostCardPropsMock } from './mock';
 
 const props = PostCardPropsMock;
 
-describe("PostCard", () => {
-    it("should render PostCard correctly", () => {
-      render(<PostCard {...props}/>);
+describe('PostCard', () => {
+  it('should render PostCard correctly', () => {
+    render(<PostCard {...props} />);
 
-      expect(screen.getByAltText(/title1/))
-      .toHaveAttribute('src', props.cover);
-      expect(screen.getByRole('heading', {name: /title1/}))
-      .toBeInTheDocument();
-      expect(screen.getByText(/body1/))
-      .toBeInTheDocument();
-    });
+    expect(screen.getByAltText(/title1/)).toHaveAttribute('src', props.cover);
+    expect(screen.getByRole('heading', { name: /title1/ })).toBeInTheDocument();
+    expect(screen.getByText(/body1/)).toBeInTheDocument();
+  });
 
-    it("should macth snapshot", () => {
-        const {container} = render(<PostCard {...props}/>);
-        expect (container.firstChild).toMatchSnapshot();
-    });
-})
+  it('should macth snapshot', () => {
+    const { container } = render(<PostCard {...props} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
