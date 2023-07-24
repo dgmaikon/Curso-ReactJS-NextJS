573efb7038ec4930e94ad9d35f44e5955cd1f026
dgmaikon <dgmaikondev@gmail.com>
2023-07-24 13:33:22 -0300

    Ref: Add propTypes and render the button.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,33 @@
-import p from 'prop-types';
-import './styles.css';
+// import p from 'prop-types';
+// import './styles.css';
 
-export const Button = ({ text, onClick, disabled = false }) => (
-  <button className="button" onClick={onClick} disabled={disabled}>
-    {text}
-  </button>
-);
+// export const Button = ({ text, onClick, disabled = false }) => (
+//   <button className="button" onClick={onClick} disabled={disabled}>
+//     {text}
+//   </button>
+// );
+
+// Button.propTypes = {
+//   text: p.string.isRequired,
+//   onClick: p.func.isRequired,
+//   disabled: p.bool,
+// };
+import P from 'prop-types';
+
+export const Button = ({ children, onButtonClick, disabled = false }) => {
+  return (
+    <button
+      disabled={disabled}
+      style={{ fontSize: '60px' }}
+      onClick={onButtonClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 Button.propTypes = {
-  text: p.string.isRequired,
-  onClick: p.func.isRequired,
-  disabled: p.bool,
+  children: P.node.isRequired,
+  onButtonClick: P.func.isRequired,
+  disabled: P.bool,
 };
