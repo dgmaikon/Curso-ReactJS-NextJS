1059a8628584e7d5846ccfb04a36c9e6ba7556b1
dgmaikon <dgmaikondev@gmail.com>
2023-07-24 13:37:41 -0300

    Ref: Change component to render and adjust some imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/global-styles.css";
-import { Home } from "./templates/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/global-styles.css';
+// import { Home } from './templates/Home';
+import { ContextAPI } from './templates/ContextAPI';
+import { CounterContextProvider } from './contexts/CounterContext';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+    <CounterContextProvider>
+      <ContextAPI />
+    </CounterContextProvider>
+  </React.StrictMode>,
 );
